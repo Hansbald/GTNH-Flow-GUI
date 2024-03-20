6ea28f40bbc4ef77a7ecabedb412ddce7d9a1895
Hansbald <hansbaldyt@gmail.com>
2024-03-20 03:46:49 +0100

    Initialization
    Gradle project with shadowJar and spotless

---
+++ src/main/java/com/hansbald/flowgui/App.java
@@ -0,0 +1,9 @@
+/* (C)2024 */
+package com.hansbald.flowgui;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/test/java/com/hansbald/flowgui/AppTest.java
@@ -0,0 +1,8 @@
+/* (C)2024 */
+package com.hansbald.flowgui;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+}
